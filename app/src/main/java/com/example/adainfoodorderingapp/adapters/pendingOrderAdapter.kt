f168fdbb9739f3135bc9031e23decbde187f9dc0
abjeet16 <avjeetyadav00@gmail.com>
2024-04-01 00:37:42 +0530

    still doing

--- app/src/main/java/com/example/adainfoodorderingapp/adapters/pendingOrderAdapter.kt
+++ app/src/main/java/com/example/adainfoodorderingapp/adapters/pendingOrderAdapter.kt
@@ -1,25 +1,34 @@
 package com.example.adainfoodorderingapp.adapters
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.adainfoodorderingapp.databinding.PendingorderlayoutBinding
 
 class pendingOrderAdapter(private val CustomerName: MutableList<String>,
-                          private val Quanity: MutableList<Int>,
-                          private val foodImage: MutableList<Int>,
-                          private val context:Context)
+                          private val Quanity: MutableList<String>,
+                          private val foodImage: MutableList<String>,
+                          private val context:Context,
+                          private val itemClicked:OnItemClicked
+    )
     :RecyclerView.Adapter<pendingOrderAdapter.ViewHolder>() {
+        interface OnItemClicked{
+            fun OnItemClickListener(position:Int)
+        }
     inner class ViewHolder(private val binding:PendingorderlayoutBinding):RecyclerView.ViewHolder(binding.root) {
         private var OrderAccepted = false
         fun bind(position: Int) {
-            val FoodImage = binding.FoodImage
             binding.apply {
                 customerName.text = CustomerName[position]
-                quantity.text = Quanity[position].toString()
-                FoodImage.setImageResource(foodImage[position])
+                quantity.text = Quanity[position]
+                val uriString = foodImage[position]
+                val uri = Uri.parse(uriString)
+                Glide.with(context).load(uri).into(binding.FoodImage)
 
                 acceptButton.apply {
                     if(!OrderAccepted){
@@ -38,6 +47,9 @@ class pendingOrderAdapter(private val CustomerName: MutableList<String>,
                             showToast("Order Dispatched")
                         }
                     }
+                }
+                binding.cardView.setOnClickListener{
+                    itemClicked.OnItemClickListener(position)
                 }
             }
         }
